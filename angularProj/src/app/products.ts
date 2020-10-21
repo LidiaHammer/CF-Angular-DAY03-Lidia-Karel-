dbaee3d02c2631c1ea8fa1b0081d65addb991771
Lidia Cekic <lidiacekic9@gmail.com>
2020-10-21 13:32:06 +0200

    test

---
+++ angularProj/src/app/products.ts
@@ -0,0 +1,17 @@
+export const products = [
+  {
+    name: 'Dog Toy',
+    price: 29,
+    description: 'The best dog toy for sensitive dogs.'
+  },
+  {
+    name: 'Cat Toy',
+    price: 19,
+    description: 'Best toys for sensitive cats.'
+  },
+  {
+    name: 'Hamster Toy',
+    price: 9,
+    description: 'The best toy for sensitive hamsters.'
+  }
+ ];
